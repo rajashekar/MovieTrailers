c139f1d08b3a634c6275db07fff636c4792d4bf5
Rajashekar Chintalapati <rchintalapati@walmartlabs.com>
2018-01-14 11:41:40 -0800

    refactor: integrating pep8 format

--- center.py
+++ center.py
@@ -1,34 +1,38 @@
-""" Create movie objects and calls fresh_tomatoes.open_movies_page to generate html """
+"""Create movie objects and
+calls fresh_tomatoes.open_movies_page to generate html
+"""
 import media
 import fresh_tomatoes
 
 # Creating movie objects
 TOY_STORY = media.Movie("Toy Story",
                         "A story of Toy",
-                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",  # noqa
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 AVATAR = media.Movie("Avatar",
                      "A marine on alien planet",
-                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",  # noqa
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 FIGHTCLUB = media.Movie("Fight Club",
                         "Mischief. Mayhem. Soap.",
-                        "https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg",
+                        "https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg",  # noqa
                         "https://www.youtube.com/watch?v=SUXWAEX2jlg")
 VFORVENDETTA = media.Movie("V for Vendetta",
                            "People should not be afraid of their governments. Governments \
                            should be afraid of their people.",
-                           "https://upload.wikimedia.org/wikipedia/en/9/9f/Vforvendettamov.jpg",
+                           "https://upload.wikimedia.org/wikipedia/en/9/9f/Vforvendettamov.jpg",  # noqa
                            "https://www.youtube.com/watch?v=jS4sVTDAsJ4")
 EVERYTHINGISILLUMINATED = media.Movie("Everything Is Illuminated",
                                       "Leave Normal Behind.",
-                                      "https://upload.wikimedia.org/wikipedia/en/2/27/Everything_Is_Illuminated_film.jpg",
-                                      "https://www.youtube.com/watch?v=l-hCtlNM32M")
+                                      "https://upload.wikimedia.org/wikipedia/en/2/27/Everything_Is_Illuminated_film.jpg",  # noqa
+                                      "https://www.youtube.com/watch?v=l-hCtlNM32M")  # noqa
 CONTACT = media.Movie("Contact",
-                      "If it's just us, it seems like an awful waste of space.",
-                      "https://upload.wikimedia.org/wikipedia/en/7/75/Contact_ver2.jpg",
+                      "If it's just us, \
+                      it seems like an awful waste of space.",
+                      "https://upload.wikimedia.org/wikipedia/en/7/75/Contact_ver2.jpg",  # noqa
                       "https://www.youtube.com/watch?v=SRoj3jK37Vc")
 
 # Keeping all above objects in a list
-MOVIES = [TOY_STORY, AVATAR, FIGHTCLUB, VFORVENDETTA, EVERYTHINGISILLUMINATED, CONTACT]
+MOVIES = [TOY_STORY, AVATAR, FIGHTCLUB,
+          VFORVENDETTA, EVERYTHINGISILLUMINATED, CONTACT]
 fresh_tomatoes.open_movies_page(MOVIES)
--- media.py
+++ media.py
@@ -1,15 +1,36 @@
 """Media module - holds Movie class """
 import webbrowser
 
+
 class Movie(object):
-    """Holds movie properties and methods """
-    # Constructor, initializes instance variables
-    def __init__(self, movie_title, movie_storyline, movie_poster, movie_trailer):
+    """ Class Movie describing the attributes of movie.
+
+        Arguments:
+            movie_title {str} -- Movie title
+            movie_storyline {str} -- Movie story line
+            movie_poster {str} -- Movie Poster
+            movie_trailer {str} -- Movie Trailer
+
+        Attributes:
+            title {str} -- Movie title
+            storyline {str} -- Movie story line
+            poster_image_url {str} -- Movie Poster
+            trailer_youtube_url {str} -- Movie Trailer
+
+        Methods:
+            show_trailer: Opens youtube movie trailer url in browser
+    """
+
+    def __init__(
+            self, movie_title, movie_storyline, movie_poster, movie_trailer):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster
         self.trailer_youtube_url = movie_trailer
 
     def show_trailer(self):
-        """ Opens up trailer"""
+        """Opens movie trailer in browser
+
+        Returns: None.
+        """
         webbrowser.open(self.trailer_youtube_url)
